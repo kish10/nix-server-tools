0bdfc3ee863680b07716e4c4f130d639742ff549
kish10 <dev.kishan@patel.blue>
2024-03-10 03:51:25 +0000

    Added versions of {age, malipace, borgbackup} utility that compiles (still need to test further).

--- utility/default.nix
+++ utility/default.nix
@@ -1,5 +1,13 @@
 {pkgs ? import <nixpkgs> {}}:
 {
+  backup = {
+    borgbackup = "";
+  };
+
+  email = {
+    mailpace = import ./email/mailpace/mailpace_utilities.nix {inherit pkgs;};
+  };
+
   encryption = {
     age = import ./encryption/age_utilities.nix {inherit pkgs;};
   };
